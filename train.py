7b3fe535b8b0ef6ff3d73bdf74150af7324765e6
isears <isaac.j.sears@gmail.com>
2025-11-04 17:26:15 +0000

    Report reprod loss and kld

--- train.py
+++ train.py
@@ -6,10 +6,12 @@ from typing import Tuple
 import torch.nn.functional as F
 import matplotlib.pyplot as plt
 from scwfae.model.ConvolutionalEcgModules import *
+from torchmetrics.aggregation import MeanMetric
+from torchmetrics import MetricCollection
 
 MAX_EPOCHS = 100
 VAL_CHECK_INTERVAL = 500
-LEARNING_RATE = 0.001
+LEARNING_RATE = 0.01
 
 
 def calculate_losses(m: ConvolutionalEcgVAE, batch: Tuple[torch.Tensor, dict]):
@@ -20,7 +22,14 @@ def calculate_losses(m: ConvolutionalEcgVAE, batch: Tuple[torch.Tensor, dict]):
     reproduction_loss = F.mse_loss(reconstruction, signal, reduction="sum")
     KLD = -0.5 * torch.sum(1 + logvar - mean.pow(2) - logvar.exp())
 
-    return reproduction_loss + KLD
+    return reproduction_loss + KLD, reproduction_loss, KLD
+
+
+def calculate_losses_for_backprop(
+    m: ConvolutionalEcgVAE, batch: Tuple[torch.Tensor, dict]
+):
+    loss, _, _ = calculate_losses(m, batch)
+    return loss
 
 
 if __name__ == "__main__":
@@ -29,12 +38,10 @@ if __name__ == "__main__":
 
     train_ds, val_ds = random_split(ds, lengths=[0.9, 0.1])
 
-    train_dl = DataLoader(train_ds, batch_size=512)
-    val_dl = DataLoader(val_ds, batch_size=1024)
+    train_dl = DataLoader(train_ds, batch_size=1024)
+    val_dl = DataLoader(val_ds, batch_size=len(val_ds))
 
-    # model = ConvolutionalEcgAutoencoder().to("cuda")
-    model = ConvolutionalEcgVAE().to("cuda")
-
+    model = ConvolutionalEcgVAE(n_filters=8, latent_dim=64).to("cuda")
     summary(model, input_data=torch.randn((32, 1, 1000)).to("cuda"))
 
     optimizer = torch.optim.AdamW(model.parameters(), lr=LEARNING_RATE)
@@ -47,7 +54,12 @@ if __name__ == "__main__":
         for batchnum, batch in enumerate(train_dl):
             if batchnum % VAL_CHECK_INTERVAL == 0:
                 model.eval()
-                val_losses = list()
+
+                val_combined_loss, val_reproduction_loss, val_kld = (
+                    MeanMetric().to("cuda"),
+                    MeanMetric().to("cuda"),
+                    MeanMetric().to("cuda"),
+                )
 
                 for val_batchnum, val_batch in enumerate(val_dl):
                     # Save a sample from the first batch
@@ -67,21 +79,30 @@ if __name__ == "__main__":
                         plt.savefig("./cache/sample.png")
                         plt.clf()
 
-                    val_losses.append(calculate_losses(model, val_batch))
+                    combined_loss, reproduction_loss, KLD = calculate_losses(
+                        model, val_batch
+                    )
 
-                avg_val_loss = sum(val_losses) / len(val_losses)
+                    val_combined_loss.update(combined_loss)
+                    val_reproduction_loss.update(reproduction_loss)
+                    val_kld.update(KLD)
 
-                if avg_val_loss < best_val_loss:
-                    best_val_loss = avg_val_loss
+                if val_combined_loss.compute() < best_val_loss:
+                    best_val_loss = val_combined_loss.compute()
 
-                    # TODO: save model somewhere
-                    print(f"Step {batchnum:04d} validation loss: {avg_val_loss:5f} (*)")
+                    print(f"Step {batchnum:04d} (*)")
+                    torch.save(model.state_dict(), "cache/savedmodels/ecgvae.pt")
+
                 else:
-                    print(f"Step {batchnum:04d} validation loss: {avg_val_loss:5f}")
+                    print(f"Step {batchnum:04d}")
+
+                print(f"\tVal Combined Loss: {val_combined_loss.compute():5f}")
+                print(f"\tVal Reproduction Loss: {val_reproduction_loss.compute():5f}")
+                print(f"\tVal KLD: {val_kld.compute():5f}")
 
                 model.train()
 
-            loss = calculate_losses(model, batch)
+            loss = calculate_losses_for_backprop(model, batch)
             optimizer.zero_grad()
             loss.backward()
             torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=4.0)
